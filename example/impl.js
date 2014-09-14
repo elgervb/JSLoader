96f06f86a318279dba369da4253721afc9aa61e4
elger <elgervb@gmail.com>
2014-09-14 22:57:12 +0200

    more examples

--- example/impl.js
+++ example/impl.js
@@ -1,18 +1,35 @@
 
 document.addEventListener('DOMContentLoaded', function () {
 
-	/**
-	 * Load JQuery from a CDN. Execute the callback when finished
-	 */
-	Loader.loadJs('https://code.jquery.com/jquery-2.1.1.js', function(){
-		console.log('JQuery JS loaded');
-	});
+
+	document.querySelector('.load').addEventListener('click', function(){
+
+		/**
+		 * Load JQuery from a CDN. Execute the callback when finished
+		 */
+		Loader.loadJs('https://code.jquery.com/jquery-2.1.1.js', function(){
+			console.log('JQuery JS loaded');
+		});
 
 
-	/**
-	 * Load JQuery css. Execute the callback when finished
-	 */
-	Loader.loadCss('https://codeorigin.jquery.com/jquery-wp-content/themes/jquery/css/base.css?v=1', function(){
-		console.log('JQuery CSS loaded');
+		/**
+		 * Load JQuery css. Execute the callback when finished
+		 */
+		Loader.loadCss('https://codeorigin.jquery.com/jquery-wp-content/themes/jquery/css/base.css?v=1', function(){
+			console.log('JQuery CSS loaded');
+		});
+
+	}, false);
+
+	document.querySelector('.unload').addEventListener('click', function(){
+
+		/**
+		 * Load JQuery css. Execute the callback when finished
+		 */
+		Loader.unloadCss(/.*codeorigin\.jquery\.com.*/, function(regex, stylesheet){
+			console.log('JQuery CSS removed ' + stylesheet);
+		});
+
 	});
+	
 });
